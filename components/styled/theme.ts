3b868f00ff812a5740ab40320174ba312056ee90
Ajay Sivan <ajayesivan@gmail.com>
2024-10-30 04:05:37 -0700

    chore: ESLint & Prettier Setup (#15)
    * ESLint
    
    * Prettier
    
    * Fix lint issues

--- components/styled/theme.ts
+++ components/styled/theme.ts
@@ -5,23 +5,23 @@ const theme: DefaultTheme = {
     textPrimary: "#FFFFFF",
     textSecondary: "#FFFFFFCC",
     primary: "#010F1D",
-    bg: "#566F81"
+    bg: "#566F81",
   },
   fontSizes: [12, 14, 18, 22, 24],
   fonts: {
-    regular: 'OpenSans-Regular',
-    bold: 'OpenSans-Bold',
-    italic: 'OpenSans-Italic',
-    boldItalic: 'OpenSans-BoldItalic'
+    regular: "OpenSans-Regular",
+    bold: "OpenSans-Bold",
+    italic: "OpenSans-Italic",
+    boldItalic: "OpenSans-BoldItalic",
   },
   activeOpacity: 0.8,
   radii: {
-    xxs: '1px',
-    xs: '2px',
-    s: '4px',
-    l: '8px',
-    xl: '12px'
-  }
+    xxs: "1px",
+    xs: "2px",
+    s: "4px",
+    l: "8px",
+    xl: "12px",
+  },
 };
 
 export default theme;
