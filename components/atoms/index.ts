31a2e38f5a10e7498e536bc7de6db5a6251f926f
Ajay Sivan <ajayesivan@gmail.com>
2024-10-31 18:22:35 +0530

    feat: movie poster component

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as MovieExpoLogo } from "./MovieExpoLogo";
 export { default as Button } from "./Button";
 export { default as TextInput } from "./TextInput";
 export { default as IconButton } from "./IconButton";
+export { default as MoviePoster } from "./MoviePoster";
--- components/styled/StyledImage.ts
+++ components/styled/StyledImage.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { Image } from "expo-image";
 import {
   layout,
   LayoutProps,
@@ -6,12 +7,17 @@ import {
   SpaceProps,
   flexbox,
   FlexboxProps,
+  border,
+  BorderProps,
 } from "styled-system";
 
-const StyledImage = styled.Image<SpaceProps & FlexboxProps & LayoutProps>`
+const StyledImage = styled(Image)<
+  SpaceProps & FlexboxProps & LayoutProps & BorderProps
+>`
   ${layout}
   ${space}
   ${flexbox}
+  ${border}
 `;
 
 export default StyledImage;
