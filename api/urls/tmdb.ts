0e60734a5a9949c822b78704e0943e7910d61b0d
Ajay Sivan <ajayesivan@gmail.com>
2024-11-03 16:17:15 +0530

    function fix

--- api/urls/tmdb.ts
+++ api/urls/tmdb.ts
@@ -4,9 +4,7 @@ export const generateTmdbImageUrl = (
   imagePath: string,
   width: number
 ): string => {
-  const imagePathWithSlash = imagePath[0] === "/" ? imagePath : "/" + imagePath;
-
-  return `${TMDB_IMAGE_URL_BASE}/w${width}${imagePathWithSlash}`;
+  return `${TMDB_IMAGE_URL_BASE}/w${width}${imagePath}`;
 };
 
 export const TMDB_MOVIE_LISTS_POPULAR = "/movie/popular";
