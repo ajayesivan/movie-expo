999d9a71bef7e3d625c067af2317ddf0e1a0f512
Ajay Sivan <ajayesivan@gmail.com>
2024-11-03 17:20:51 +0530

    Button Loading animation

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,14 +1,41 @@
 import { StyledText, StyledTouchableOpacity } from "@/components/styled";
-import React from "react";
+import React, { useEffect } from "react";
+import RemixIcon from "react-native-remix-icon";
+import { useTheme } from "styled-components/native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
 
 interface ButtonProps {
   label: string;
   onPress: () => void;
+  isLoading: boolean;
   type?: "primary" | "link";
 }
 
-const Button = ({ label, onPress, type = "primary" }: ButtonProps) => {
+const Button = ({
+  label,
+  onPress,
+  type = "primary",
+  isLoading,
+}: ButtonProps) => {
   const isPrimary = type === "primary";
+  const theme = useTheme();
+
+  const rotation = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ rotate: `${rotation.value}deg` }],
+    };
+  });
+
+  useEffect(() => {
+    rotation.value = withRepeat(withTiming(360, { duration: 2000 }), -1);
+  }, [rotation]);
 
   return (
     <StyledTouchableOpacity
@@ -20,12 +47,21 @@ const Button = ({ label, onPress, type = "primary" }: ButtonProps) => {
       onPress={onPress}
       alignItems="center"
     >
-      <StyledText
-        fontFamily="bold"
-        color={isPrimary ? "textPrimary" : "primary"}
-      >
-        {label}
-      </StyledText>
+      {isLoading ? (
+        <Animated.View style={animatedStyle}>
+          <RemixIcon
+            name="ri-loader-3-line"
+            color={theme.colors[isPrimary ? "textPrimary" : "primary"]}
+          />
+        </Animated.View>
+      ) : (
+        <StyledText
+          fontFamily="bold"
+          color={isPrimary ? "textPrimary" : "primary"}
+        >
+          {label}
+        </StyledText>
+      )}
     </StyledTouchableOpacity>
   );
 };
